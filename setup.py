76e6d20b585aa61e89885f3894d60947f43942ca
Jeremy Dunck <jdunck@gmail.com>
2014-04-14 18:33:23 -0500

    setup, manifest, tox

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open('README.rst', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='duplo',
+    version='1.0.0',
+    description='Makes testing easier',
+    long_description=long_description,
+    author='Jeremy Dunck',
+    author_email='jdunck@gmail.com',
+    url='https://github.com/jdunck/duplo',
+    packages=['duplo'],
+    install_requires=['importlib>=1.0.3,<1.2.0'],
+    classifiers=[]
+)
